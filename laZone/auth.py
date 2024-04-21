d3ed833fb9d7cc35a9eef109911e90fb8e6a18d1
abryckaert <bryckaert.anthony@gmail.com>
2024-04-21 21:50:23 +0200

    Ajout d'un mecanisme de défense contre le bruteforce

--- laZone/auth.py
+++ laZone/auth.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, redirect, render_template, request, flash, url_for
+from datetime import datetime, timedelta, timezone
+from flask import Blueprint, redirect, render_template, request, flash, session, url_for
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
@@ -8,21 +9,39 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if 'login_attempts' not in session:
+        session['login_attempts'] = 0
+        session['lockout_time'] = None
+
+    current_time = datetime.now(timezone.utc) 
+    lockout_time = session.get('lockout_time')
+
+    if lockout_time and current_time < lockout_time:
+        remaining_lockout_minutes = (lockout_time - current_time).total_seconds() / 60
+        flash(f'Accès temporairement bloqué en raison de trop de tentative échouée. Re essayez dans {int(remaining_lockout_minutes)} minutes.', 'error')
+        return render_template("login.html")
+
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
         user = User.query.filter_by(email=email).first()
 
         if user and check_password_hash(user.password, password):
+            session['login_attempts'] = 0
+            session['lockout_time'] = None
             login_user(user, remember=True)
-            flash('Logged in successfully!', category='success')
-            return redirect(url_for('views.home'))      
-        elif not user:
-            flash('Email does not exist.', category='error')
+            return redirect(url_for('views.home'))
         else:
-            flash('Incorrect password, try again.', category='error')
+            session['login_attempts'] += 1
+            if session['login_attempts'] >= 3:
+                lockout_minutes = 5 * 2 ** (session['login_attempts'] - 3)  
+                session['lockout_time'] = current_time + timedelta(minutes=lockout_minutes)
+                flash(f'Trop de tentatives échouées. Veuillez réessayer dans {lockout_minutes} minutes.', 'error')
+            else:
+                flash('Email ou mot de passe incorrect.', 'error')
 
-    return render_template("login.html", user=current_user)
+
+    return render_template("login.html")
 
 @auth.route('/logout')
 @login_required
