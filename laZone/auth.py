38b2343c02f6c075ae6ad70bd20d4f63fed6d48f
najiro64 <bryckaert.anthony@gmail.com>
2024-05-03 16:47:35 +0200

    Ameliore la sécurité de l'authentification

--- laZone/auth.py
+++ laZone/auth.py
@@ -1,47 +1,48 @@
 from datetime import datetime, timedelta, timezone
-from flask import Blueprint, redirect, render_template, request, flash, session, url_for
+from .extensions import limiter
+from flask import Blueprint, app, redirect, render_template, request, flash, session, url_for, current_app
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from .extensions import db
 
+
 auth = Blueprint('auth', __name__)
+
+login_attempts_by_ip = {}
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    if 'login_attempts' not in session:
-        session['login_attempts'] = 0
-        session['lockout_time'] = None
+    ip = request.remote_addr  
+    if ip not in login_attempts_by_ip:
+        login_attempts_by_ip[ip] = {'count': 0, 'lockout_time': None}
 
-    current_time = datetime.now(timezone.utc) 
-    lockout_time = session.get('lockout_time')
-
-    if lockout_time and current_time < lockout_time:
-        remaining_lockout_minutes = (lockout_time - current_time).total_seconds() / 60
-        flash(f'Accès temporairement bloqué en raison de trop de tentative échouée. Re essayez dans {int(remaining_lockout_minutes)} minutes.', 'error')
-        return render_template("login.html")
+    if login_attempts_by_ip[ip]['lockout_time']:
+        if datetime.now() < login_attempts_by_ip[ip]['lockout_time']:
+            lockout_minutes = (login_attempts_by_ip[ip]['lockout_time'] - datetime.now()).total_seconds() / 60
+            flash(f'Please retry in {int(lockout_minutes)} minutes due to repeated login failures.', 'error')
+            return render_template("login.html")
 
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
+        
         user = User.query.filter_by(email=email).first()
 
         if user and check_password_hash(user.password, password):
-            session['login_attempts'] = 0
-            session['lockout_time'] = None
+            login_attempts_by_ip[ip] = {'count': 0, 'lockout_time': None}  
             login_user(user, remember=True)
-            return redirect(url_for('views.home'))
+            return redirect(url_for('views.home'))  
         else:
-            session['login_attempts'] += 1
-            if session['login_attempts'] >= 3:
-                lockout_minutes = 5 * 2 ** (session['login_attempts'] - 3)  
-                session['lockout_time'] = current_time + timedelta(minutes=lockout_minutes)
-                flash(f'Trop de tentatives échouées. Veuillez réessayer dans {lockout_minutes} minutes.', 'error')
-            else:
-                flash('Email ou mot de passe incorrect.', 'error')
-
+            login_attempts_by_ip[ip]['count'] += 1
+            if login_attempts_by_ip[ip]['count'] >= 3:
+                lockout_minutes = 5 * (2 ** (login_attempts_by_ip[ip]['count'] - 3))  
+                login_attempts_by_ip[ip]['lockout_time'] = datetime.now() + timedelta(minutes=lockout_minutes)
+            flash('Invalid credentials. Please try again.', 'error')
 
     return render_template("login.html")
+
+
 
 @auth.route('/logout')
 @login_required
@@ -72,7 +73,7 @@ def sign_up():
             new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1), profil_image_url="/static/userImages/default.png")
             db.session.add(new_user)
             db.session.commit()
-            login_user(new_user, remember=True)  # Automatically log in the new user
+            login_user(new_user, remember=True)  
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
     
